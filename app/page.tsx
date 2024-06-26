6f1bc5e610fadf1178566103886374a93f89878d
MSI\becon <becontinue17@gmail.com>
2024-06-26 13:15:24 +0700

    Your commit message

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,119 @@
-import Image from "next/image";
+'use client'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Image from 'next/image';
+import Link from 'next/link';
+
+interface Pokemon {
+  name: string;
+  sprites: {
+    other: {
+      home: {
+        front_default: string;
+      };
+    };
+  };
+  abilities: { ability: { name: string } }[];
+  url: string;
+}
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [number, setNumber] = useState(0);
+  const [activeSearch, setActiveSearch] = useState("");
+  const [allPokemons, setAllPokemons] = useState<Pokemon[]>([]);
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  useEffect(() => {
+    let abortController = new AbortController();
+
+    const loadPokemons = async () => {
+      try {
+        setLoading(true);
+        setError("");
+
+        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=20&offset=${number}`);
+
+        const pokemonResults = await Promise.all(res.data.results.map(async (pokemon: { url: string }) => {
+          const pokemonRes = await axios.get(pokemon.url);
+          return pokemonRes.data;
+        }));
+
+        setAllPokemons(pokemonResults);
+
+        const filteredPokemons = pokemonResults.filter((pokemon: Pokemon) => 
+          pokemon.name.toLowerCase().includes(activeSearch.toLowerCase())
+        );
+
+        setPokemons(filteredPokemons);
+      } catch (error) {
+        setError("Error fetching data");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadPokemons();
+    return () => abortController.abort();
+  }, [number, activeSearch]);
+
+  const prevPokemon = () => {
+    if (number > 0) setNumber(number - 20);
+  };
+
+  const nextPokemon = () => {
+    setNumber(number + 20);
+  };
+
+  const handleActiveSearch = (value: string) => {
+    setActiveSearch(value);
+    const filteredPokemons = allPokemons.filter((pokemon: Pokemon) => 
+      pokemon.name.toLowerCase().includes(value.toLowerCase())
+    );
+    setPokemons(filteredPokemons);
+  };
+
+  return (
+    <main className="p-4">
+      {/* Search input */}
+      <div className="input input-bordered flex items-center gap-2">
+        <input 
+          type="text" 
+          className="grow" 
+          placeholder="Search Pokemon By Name" 
+          value={activeSearch}
+          onChange={(e) => handleActiveSearch(e.target.value)}
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <br />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      {/* Pagination buttons */}
+      <div className="flex justify-between mb-4">
+        <button className="btn" onClick={prevPokemon} disabled={number <= 0}> Previous </button>
+        <button className="btn" onClick={nextPokemon}> Next </button>
+      </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      {/* Pokémon cards */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {pokemons.length > 0 && pokemons.map((pokemon, index) => (
+          <Link href={`/detail/${pokemon.name}`} key={index}>
+            <div className="card w-full bg-base-100 shadow-xl cursor-pointer">
+              <figure>
+                <Image
+                  src={pokemon.sprites.other.home.front_default}
+                  alt={pokemon.name}
+                  width={500}
+                  height={500}
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title text-center">{pokemon.name}</h2>
+              </div>
+            </div>
+          </Link>
+        ))}
       </div>
     </main>
   );
